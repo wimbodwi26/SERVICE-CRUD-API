7ef3e5d24e06d6b5a01ecab8549da1edaffaad94
wimbodwi26 <wimbodwin@gmail.com>
2025-08-01 17:39:56 +0700

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -12,7 +12,7 @@ func SetupRouter() *gin.Engine {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:   []string{"*"},
+		AllowOrigins:   true,
 		AllowMethods: 	[]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders: 	[]string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders: 	[]string{"Content-Length"},
